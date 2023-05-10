56b3fb7cf8bb17466fb1cde28713b645b36115d2
FranciscoTurro <franciscogtrro@outlook.com.ar>
2023-05-10 11:30:51 -0300

    adding posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,14 @@ import { type NextPage } from "next";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const { data } = api.posts.getAll.useQuery();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white"></main>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      {data?.map((post) => (
+        <div key={post.id}>{post.content}</div>
+      ))}
+    </main>
   );
 };
 
